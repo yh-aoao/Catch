8da3a65fe5cf22c388f133af01a6d7f846979a19
yh-aoao <2949158881@qq.com>
2026-08-01 17:48:09 +0800

    fix: throw_basket球在手中不因手接触而提前终止

--- catch_it_copy/gym_dcmm/envs/DcmmVecEnv.py
+++ catch_it_copy/gym_dcmm/envs/DcmmVecEnv.py
@@ -2050,7 +2050,10 @@ class DcmmVecEnv(gym.Env):
                         self.terminated = out_of_bounds
 
                 elif self.task == "Tracking":
-                    if self.object_motion == "throw":
+                    if self.object_motion == "throw_basket":
+                        # 抛球入篮：球在手中，不因手接触而终止。只检查底盘碰撞
+                        self.terminated = np.any(mask_coll)
+                    elif self.object_motion == "throw":
                         # 保持原有抛射模式逻辑
                         self.terminated = np.any(mask_coll) or np.any(mask_finger)
                     else:

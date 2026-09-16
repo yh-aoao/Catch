ef8d18795935c37485d275932b35bb4592e72127
yh-aoao <2949158881@qq.com>
2026-09-16 23:56:54 +0800

    修改roll跟basket问题

--- catch_it_copy/gym_dcmm/envs/roll_compat.py
+++ catch_it_copy/gym_dcmm/envs/roll_compat.py
@@ -8,6 +8,10 @@ def make_roll_645edc4(parameters):
     from gym_dcmm.envs.DcmmVecEnv_roll_645edc4 import DcmmVecEnv
     env = DcmmVecEnv(**parameters)
     if verbose:
+        import inspect
+        import configs.env.DcmmCfg_roll_645edc4 as cfg
         print('[roll-baseline] commit=645edc4e21631683254fba51cc66b035b409518d '
               'environment=original; execution_guard_v1 is not active', flush=True)
+        print('[roll-baseline-source] environment={} config={}'.format(
+            inspect.getfile(DcmmVecEnv), cfg.__file__), flush=True)
     return env
